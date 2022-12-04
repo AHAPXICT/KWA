57393feb3fab332a16708bac10406428b43e5422
Анархист <118834993+AHAPXICT@users.noreply.github.com>
2022-12-04 14:54:28 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число");
+        double one = double.Parse(Console.ReadLine());
+        double two = double.Parse(Console.ReadLine());
+        double three = double.Parse(Console.ReadLine());
+        double D = Math.Pow(two, 2) - 4 * one * three;
+        double x1 = (-two - Math.Sqrt(D)) / (2 * one);
+        double x2 = (-two + Math.Sqrt(D)) / (2 * one);
+        Console.WriteLine("х1 = {0}, х2 = {1}", x1, x2);
+    }
+}
